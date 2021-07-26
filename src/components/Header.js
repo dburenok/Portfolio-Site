4a9ed63a2fd64533f28cdbf1cdc8cb53cd9ea974
Dmitriy Burenok <dmitriyburenok@gmail.com>
2021-07-25 21:44:16 -0700

    new design

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ function Header() {
     <div className="about">
       <div>
         <ul style={{ display: "flex", listStyle: "none", justifyContent: "center", alignItems: "center" }}>
-          <li><img src={me} style={{ width: "80px", borderRadius: "35px" }} alt="Dmitriy Burenok" /></li>
-          <li><h1>DMITRIY BURENOK</h1></li>
+          <li><img src={me} style={{ width: "80px", borderRadius: "100%" }} alt="Dmitriy Burenok" /></li>
+          <li><h1>Dmitriy Burenok</h1></li>
         </ul>
-        <h2>Computer Science and Mathematics @ UBC</h2>
+        <h2 style={{ textAlign: "center", display: "block", margin: "0 auto" }}>&#123; role: "Development Support at <a href="https://www.keywordsstudios.com/">Keywords Studios</a>",<br />major: "UBC Computer Science" &#125;</h2><br />
         <Socials />
       </div>
     </div>
