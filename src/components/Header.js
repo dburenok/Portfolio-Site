e0d23066340e3bff496e636779ff96b969ad09b2
Dmitriy Burenok <dmitriyburenok@gmail.com>
2021-07-26 22:03:46 -0700

    shadow on dp

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div className="about">
       <div>
         <ul style={{ display: "flex", listStyle: "none", justifyContent: "center", alignItems: "center" }}>
-          <li><img src={me} style={{ width: "80px", borderRadius: "100%" }} alt="Dmitriy Burenok" /></li>
+          <li><img src={me} style={{ width: "80px", borderRadius: "100%", boxShadow: "3px 5px 10px #b7b7b7" }} alt="Dmitriy Burenok" /></li>
           <li><h1>Dmitriy Burenok</h1></li>
         </ul>
         <h2 style={{ textAlign: "center", display: "block", margin: "0 auto" }}>Development Support at <a href="https://www.keywordsstudios.com/">Keywords Studios</a>,<br />UBC Computer Science</h2><br />
