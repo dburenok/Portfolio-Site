3a9a838d11e2262a31f827dfcdba6fa939715560
Dmitriy Burenok <dmitriyburenok@Dmitriys-MacBook-Pro.local>
2021-10-05 19:13:41 -0700

    role change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,18 @@ import me from '../img/me.jpg';
 import Socials from './Socials';
 
 function Header() {
-  return (
-    <div className="about">
-      <div>
-        <ul style={{ display: "flex", listStyle: "none", justifyContent: "center", alignItems: "center" }}>
-          <li><img src={me} style={{ width: "80px", borderRadius: "100%", boxShadow: "3px 5px 10px #b7b7b7" }} alt="Dmitriy Burenok" /></li>
-          <li><h1>Dmitriy Burenok</h1></li>
-        </ul>
-        <h2 style={{ textAlign: "center", display: "block", margin: "0 auto" }}>Development Support at <a href="https://www.keywordsstudios.com/">Keywords Studios</a>,<br />UBC Computer Science</h2><br />
-        <Socials />
-      </div>
-    </div>
-  )
+	return (
+		<div className="about">
+			<div>
+				<ul style={{ display: "flex", listStyle: "none", justifyContent: "center", alignItems: "center" }}>
+					<li><img src={me} style={{ width: "80px", borderRadius: "100%", boxShadow: "3px 5px 10px #b7b7b7" }} alt="Dmitriy Burenok" /></li>
+					<li><h1>Dmitriy Burenok</h1></li>
+				</ul>
+				<h2 style={{ textAlign: "center", display: "block", margin: "0 auto" }}>Computer Science Teaching Assistant @ <a href="https://www.cs.ubc.ca">UBC</a></h2><br />
+				<Socials />
+			</div>
+		</div>
+	)
 }
 
 export default Header
