5c22e82bfc433c3e028186d018dd9692ff90168c
Dmitriy Burenok <dmitriyburenok@gmail.com>
2022-01-19 00:54:50 -0800

    fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import me from '../img/me.jpg';
+import dp from '../img/dp.png';
 import Socials from './Socials';
 
 function Header() {
 	return (
 		<div className="about">
 			<div style={{ display: "flex", listStyle: "none", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-				<img src={me} style={{ width: "100px", borderRadius: "30px" }} alt="selfie" />
+				<img src={dp} style={{ width: "100px", borderRadius: "30px" }} alt="selfie" />
 				<h1>dmitriy burenok</h1>
 				<h2 style={{ textAlign: "center", display: "block", margin: "0 auto" }}>computer science @ <a href="https://www.cs.ubc.ca">ubc</a></h2><br />
 				<Socials />
